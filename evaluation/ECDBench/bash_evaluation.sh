04789657354f1df38363bacef026dca6f6b50411
yuweiyang-anu <yuwei.yang@anu.edu.au>
2025-08-07 14:29:46 +0800

    Add files via upload

---
+++ evaluation/ECDBench/bash_evaluation.sh
@@ -0,0 +1,40 @@
+### You need to replace the 'your_trained_model_path' of the actual model path and activate the corresponding conda enironment for different model evaluation
+
+## Evaluation scripts for llava_next
+python inference_on_ecdbench_llava_next.py --model_type 'base' --save_name 'original_llava_next' --model_path ''
+
+python inference_on_reachqa_llava_next.py --model_type 'finetuning' --save_name 'llava_next_on_ECD' --model_path 'your_trained_model_path'
+
+python inference_on_ecdbench_llava_next.py --model_type 'finetuning' --save_name 'llava_next_on_ECD' --model_path 'your_trained_model_path'
+
+python evaluate_on_ecdbench.py --infer_data_path './infer_results/original_llava_next/gen-llama3-llava-next-8b-hf-original_llava_next.json' --output_file './infer_results/original_llava_next/score-llama3-llava-next-8b-hf-original_llava_next.json'
+
+python evaluate_on_ecdbench.py --infer_data_path './infer_results/llava_next_on_ECD/gen-llama3-llava-next-8b-hf-llava_next_on_ECD.json' --output_file './infer_results/llava_next_on_ECD/score-llama3-llava-next-8b-hf-llava_next_on_ECD.json'
+
+## Evaluation scripts for minicpm_v2_6
+python inference_on_ecdbench_minicpm_v2_6.py --model_type 'base' --save_name 'original_minicpm_v2_6' --model_path ''
+
+python inference_on_ecdbench_minicpm_v2_6.py --model_type 'finetuning' --save_name 'minicpm_v2_6_on_ECD' --model_path 'your_trained_model_path'
+
+python evaluate_on_ecdbench.py --infer_data_path './infer_results/original_minicpm_v2_6/gen-minicpm-v2_6-original_minicpm_v2_6.json' --output_file './infer_results/original_minicpm_v2_6/score-minicpm-v2_6-original_minicpm_v2_6.json'
+
+python evaluate_on_ecdbench.py --infer_data_path './infer_results/minicpm_v2_6_on_ECD/gen-minicpm-v2_6-minicpm_v2_6_on_ECD.json' --output_file './infer_results/minicpm_v2_6_on_ECD/score-minicpm-v2_6-minicpm_v2_6_on_ECD.json'
+
+## Evaluation scripts for phi3v
+pip install transformers==4.47.0
+python inference_on_ecdbench_phi3v.py --model_type 'base' --save_name 'original_phi3v' --model_path ''
+
+python inference_on_ecdbench_phi3v.py --model_type 'finetuning' --save_name 'full_phi3v_on_ECD' --model_path 'your_trained_model_path'
+
+python evaluate_on_ecdbench.py --infer_data_path './infer_results/original_phi3v/gen-phi-3-vision-original_phi3v.json' --output_file './infer_results/original_phi3v/score-phi-3-vision-original_phi3v.json'
+
+python evaluate_on_ecdbench.py --infer_data_path './infer_results/full_phi3v_on_ECD/gen-phi-3-vision-full_phi3v_on_ECD.json' --output_file './infer_results/full_phi3v_on_ECD/score-phi-3-vision-full_phi3v_on_ECD.json'
+
+## Evaluation scripts for qwen2_5_vl
+python inference_on_ecdbench_qwen2_5_vl.py --model_type 'base' --save_name 'original_qwen2_5_vl' --model_path ''
+
+python inference_on_ecdbench_qwen2_5_vl.py --model_type 'finetuning' --save_name 'lora_qwen2_5_vl_on_ECD' --model_path 'your_trained_model_path'
+
+python evaluate_on_ecdbench.py --infer_data_path './infer_results/original_qwen2_5_vl/gen-qwen2.5-vl-7b-instruct-original_qwen2_5_vl.json' --output_file './infer_results/original_qwen2_5_vl/score-qwen2.5-vl-7b-instruct-original_qwen2_5_vl.json'
+
+python evaluate_on_ecdbench.py --infer_data_path './infer_results/lora_qwen2_5_vl_on_ECD/gen-qwen2.5-vl-7b-instruct-lora_qwen2_5_vl_on_ECD.json' --output_file './infer_results/lora_qwen2_5_vl_on_ECD/score-qwen2.5-vl-7b-instruct-lora_qwen2_5_vl_on_ECD.json'
---
+++ evaluation/ECDBench/inference_on_ecdbench_llava_next.py
@@ -0,0 +1,91 @@
+import os
+import re
+import json
+import torch
+import argparse
+from tqdm import tqdm
+from PIL import Image
+from transformers import LlavaNextProcessor, LlavaNextForConditionalGeneration
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # input/output
+    parser.add_argument('--model_type', type=str, default='ori', required=True)
+    parser.add_argument('--save_name', type=str, default='ori', required=True)
+    parser.add_argument('--model_path', type=str)
+    args = parser.parse_args()
+
+    if args.model_type == 'finetuning':
+        processor = LlavaNextProcessor.from_pretrained(args.model_path)
+        model = LlavaNextForConditionalGeneration.from_pretrained(
+            args.model_path, torch_dtype=torch.float16, device_map="auto")
+    else:
+        processor = LlavaNextProcessor.from_pretrained(
+            "llava-hf/llama3-llava-next-8b-hf")
+        model = LlavaNextForConditionalGeneration.from_pretrained(
+            "llava-hf/llama3-llava-next-8b-hf",
+            torch_dtype=torch.float16,
+            device_map="auto")
+
+    model_name = 'llama3-llava-next-8b-hf'
+
+    temperature = 0
+    result_list = []
+    output_dir = f'./infer_results/{args.save_name}'
+
+    input_file = 'ECD/public_benchmarks/ECDBench/ECD_Bench_All.json'
+    print(f"Reading {input_file}...")
+    with open(input_file) as f:
+        data = json.load(f)
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    output_file = os.path.join(output_dir,
+                               f'gen-{model_name}-{args.save_name}.json')
+
+    for k in tqdm(range(len(data))):
+        json_object = {}
+        prompt = data[k]['question']
+        image_path = 'ECD/public_benchmarks/ECDBench/rendered_images' + data[
+            k]["image_id"]
+        image = Image.open(image_path).convert('RGB')
+        json_object["question"] = prompt
+        json_object["gt_answer"] = data[k]['answer']
+        json_object["split"] = data[k]["split"]
+
+        conversation = [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": prompt
+                    },
+                    {
+                        "type": "image"
+                    },
+                ],
+            },
+        ]
+        prompt = processor.apply_chat_template(conversation,
+                                               add_generation_prompt=True)
+
+        inputs = processor(images=image, text=prompt,
+                           return_tensors="pt").to(model.device)
+
+        # autoregressively complete prompt
+        output = model.generate(**inputs, max_new_tokens=100)
+        text_output = processor.decode(output[0], skip_special_tokens=True)
+        response = re.search(r'(?<=assistant)(.*)', text_output,
+                             re.DOTALL).group(1).strip()
+        print('===response:===', response)
+
+        json_object["pred_answer"] = response
+
+        result_list.append(json_object)
+
+        print(f"Saving results to {output_file}...")
+        os.makedirs(os.path.dirname(output_file), exist_ok=True)
+        with open(output_file, "w+") as f:
+            json.dump(result_list, f, indent=4)
+        print(f"Results saved.")
